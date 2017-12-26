e62eeecb815afdf9f7ff0446daa573953dd77bc6
Dylan Sather <dylan.sather@gmail.com>
2017-12-26 12:53:37 -0700

    Updating config template

--- config/config.template.js
+++ config/config.template.js
@@ -1,6 +1,17 @@
 const config = {
+  maps: {
+    sanFrancisco: {
+      latitude: 37.7577,
+      longitude: -122.4376,
+      zoom: 11.5,
+      bearing: 0,
+      pitch: 0,
+      width: 700,
+      height: 700,
+    },
+  },
   mapbox: {
-    accessToken: 'access token here',
+    accessToken: 'your access token here',
   },
 };
 
